d9d11a852006fe6998f0b87921313475e61e18e1
Utkarsh <78083867+hsraktu17@users.noreply.github.com>
2024-06-26 20:37:59 +0530

    dropdown fixed

--- components/Appbar.tsx
+++ components/Appbar.tsx
@@ -39,7 +39,7 @@ export default function Appbar() {
                             <path d="M15.6666 27H10.3333M20.9999 9.66669C20.9999 7.54496 20.1571 5.51012 18.6568 4.00983C17.1565 2.50954 15.1217 1.66669 12.9999 1.66669C10.8782 1.66669 8.84338 2.50954 7.34309 4.00983C5.8428 5.51012 4.99994 7.54496 4.99994 9.66669C4.99994 13.7869 3.96057 16.608 2.7995 18.4739C1.82011 20.0479 1.33042 20.8348 1.34838 21.0544C1.36826 21.2975 1.41976 21.3902 1.61564 21.5355C1.79256 21.6667 2.59006 21.6667 4.18508 21.6667H21.8148C23.4098 21.6667 24.2073 21.6667 24.3842 21.5355C24.5801 21.3902 24.6316 21.2975 24.6515 21.0544C24.6695 20.8348 24.1798 20.0479 23.2004 18.4739C22.0393 16.608 20.9999 13.7869 20.9999 9.66669Z" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                         </svg>
                         <Image src={image} alt="Profile" className="rounded-full w-8 h-8"/>
-                        <svg width="12" height="8" viewBox="0 0 12 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <svg width="12" height="8" viewBox="0 0 12 8" fill="none" xmlns="http://www.w3.org/2000/svg" className=" relative right-[0.5rem] top-[0.55rem]">
                             <path d="M1 1.5L6 6.5L11 1.5" stroke="black" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"/>
                         </svg>
                     </div>
